54077a7626cde08307e09b9c87cd07015869db16
tcdw <tcdw2011@gmail.com>
2018-06-01 20:34:42 +0800

    修正依赖更新的一些问题

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,23 @@
+import progress from 'rollup-plugin-progress';
 import buble from 'rollup-plugin-buble';
 import postcss from 'rollup-plugin-postcss';
-import uglify from 'rollup-plugin-uglify';
+import { uglify } from 'rollup-plugin-uglify';
 import autoprefixer from 'autoprefixer';
 import clean from 'postcss-clean';
 
 const isDist = process.env.BUILD === 'production';
 
-const buildPlan = {
-    input: 'source/js/index.js',
+export default {
+    input: 'src/js/index.js',
     output: {
         file: 'dist/bundle.js',
         format: 'iife',
         sourcemap: !isDist,
     },
     plugins: [
+        progress({
+            clearLine: false,
+        }),
         postcss({
             extract: true,
             sourceMap: !isDist,
@@ -32,5 +36,3 @@ const buildPlan = {
         uglify(),
     ],
 };
-
-export default buildPlan;
